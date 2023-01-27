76eb6c08d653379e32f18e373af62c5d52507fd0
changmin <opopem2@gmail.com>
2023-01-27 18:52:50 +0900

    c
    c

--- CMS SYSTEM/Assets/Script/TestScript.cs
+++ CMS SYSTEM/Assets/Script/TestScript.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,11 +10,28 @@ public class TestScript : MonoBehaviour
 {
     public GameObject OriginalPrefab;
     public Transform Parent;
+    public GameObject[] clone = new GameObject[30];
 
+    int[] NumMax = new int[30];
+    public Text[] NumberText;
+    int i = 1;
 
+    void Start()
+    {
+        for (int k=0; k < NumMax.Length; k++)
+        {
+            NumMax[k] = k+1;
+        }
+    }
     public void PrefabAddBtn()
     {
-        NumberTrans.numbertrans();
-        Instantiate(OriginalPrefab, Parent);
+
+        clone[i] = Instantiate(OriginalPrefab, Parent);
+        clone[i].name = "clone"+ NumMax[i].ToString();
+        NumberText[i] = clone[i].GetComponent<Text>();
+      //  NumberText[i].text = NumMax[i].ToString() + ".";
+        ++i;
+        Debug.Log("d");
+
     }
 }
